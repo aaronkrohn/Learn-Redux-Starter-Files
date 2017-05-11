32c0917e93134fb40b760564393a45498bbdf532
aaron krohn <ajayballer@hotmail.com>
2017-05-11 17:40:53 +0100

    Make posts likes increment

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render(){
     return (
-      <div className="photogrid-photo">
-        PhotoGrid
+      <div className="photo-grid">
+        {this.props.posts.map((post, i)=> <Photo {...this.props} post={post} i={i} key={i} />)}
       </div>
     )
   }
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,6 +1,25 @@
 function posts(state = [], action){
   console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      // return state.map((post, i) => {
+      //   if(action.index !== i){
+      //     return post;
+      //   }
+      //   post.likes++
+      //   return post;
+      // });
+      const i = action.index;
+      return [
+        ...state.slice(0, i), // Get copy of everything before;
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1) // Get copy of everything after
+      ];
+      break;
+    default:
+      return state;
+  }
+
 }
 
 export default posts
